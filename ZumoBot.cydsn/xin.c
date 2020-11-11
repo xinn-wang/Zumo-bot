b8419081efb8cb7124492e6c2470743be1aa539e
XinWang <xin.wang@metropolia.fi>
2020-11-11 12:15:34 +0200

    Week 4 assignment 2 (need to debug together)

--- ZumoBot.cydsn/xin.c
+++ ZumoBot.cydsn/xin.c
@@ -14,8 +14,7 @@
 #define STOP 5
 
 //Week3_assignment_1
-void w3_a1(void)
-{
+void w3_a1(void){
     printf("Please put the robort to the start line");
     motor_start();      
     motor_forward(0,0);
@@ -43,8 +42,8 @@ void w3_a1(void)
 
 }   
 
-void w3_a2(void)
-{
+
+void w3_a2(void){
     printf("Robot starts!");
     Ultra_Start();                          // Ultra Sonic Start function
     motor_start();      
@@ -56,12 +55,12 @@ void w3_a2(void)
     printf("Light off");
     BatteryLed_Write(0);
     vTaskDelay(1000);
-    
-    
+     
     
     while (SW1_Read()==1){
         int d = Ultra_GetDistance();
         if (d < 10){
+            motor_forward(0,0);
             motor_backward(100,800);
             motor_turn(250,0,350);
         }
@@ -71,8 +70,8 @@ void w3_a2(void)
     motor_stop();
 }
 
-void w3_a3(void)
-{
+
+void w3_a3(void){
     printf("Robot starts!");
     Ultra_Start();                          // Ultra Sonic Start function
     motor_start();      
@@ -151,6 +150,59 @@ void w4_a1(void){
     }
 
 }
+void w4_a2(void){
+    printf("Please put the robort at the start line\n");
+    struct sensors_ dig;
+    reflectance_set_threshold(9000, 9000, 11000, 11000, 9000, 9000);
+    
+    reflectance_start();
+    IR_Start();
+    motor_start();
+    motor_forward(0,0);
+    
+    BatteryLed_Write(1);
+    while (SW1_Read()==1);
+    printf("Move starts");
+    BatteryLed_Write(0);
+    vTaskDelay(1000);
+    
+    int angle=0;
+    
+    IR_wait();
+    reflectance_digital(&dig);
+    
+    
+    while (dig.L1 == 1 && dig.R1 == 1){
+        motor_forward(70,0);
+        reflectance_digital(&dig);
+        if(dig.L2 == 1 && dig.R2 == 0){ 
+            angle += 2;
+            tank_turn(angle);
+
+        }
+        if(dig.L2 == 0 && dig.R2 == 1){
+            angle -= 2;
+            tank_turn(angle);
+            
+        }
+        if(dig.L1 == 0){
+            angle -= 1;
+            tank_turn(angle);
+            
+        }
+        if(dig.R1 == 0){
+            angle += 1;
+            tank_turn(angle);
+           
+        }
+       
+    }
+  
+    
+    motor_stop();
+
+}
+
 
 void tank_turn(int16 angle){
      uint8 left_wheel = 0,right_wheel =0;
